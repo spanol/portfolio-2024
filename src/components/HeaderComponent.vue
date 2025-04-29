d67ebd76ce10962475988c9f2705cba45889b8c9
spanol <cspanol@hotmail.com.br>
2025-04-29 01:57:48 -0300

    feat: enhance HeaderComponent and HomeView styling; improve color transitions and layout responsiveness

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,33 +1,45 @@
 <template>
-  <header class="z-50 fixed top-0 w-full shadow-md h-16 dark:bg-dark-background dark:text-dark-text">
+  <header
+    class="z-50 fixed top-0 w-full shadow-md h-16 dark:bg-dark-background dark:text-dark-text"
+  >
     <div
       class="h-full bg-white dark:bg-dark-surface flex items-center px-5 lg:px-16 justify-between font-extrabold text-lg"
-      :class="{ 'border-b dark:border-dark-border': isOpen }">
-      <RouterLink class="flex items-center gap-x-2 hover:text-blue-500" to="/">
-        <!-- <img :src="isDarkMode ? '/favicon-dark.svg' : '/favicon-light.svg'" alt="Logo" class="h-10 w-10 rounded-full" /> -->
+      :class="{ 'border-b dark:border-dark-border': isOpen }"
+    >
+      <RouterLink class="flex items-center gap-x-2 text-primary" to="/">
         spanol.dev
       </RouterLink>
 
       <div class="hidden md:flex justify-between items-center gap-x-5">
-        <RouterLink v-for="(tab, index) in navTabs" :key="index" :to="tab.path"
-          :class="{ 'border-b-4 border-blue-500 dark:border-blue-400': $route.path === tab.path }"
-          class="hover:text-blue-500 dark:hover:text-blue-400 transition-all duration-300">
+        <RouterLink
+          v-for="(tab, index) in navTabs"
+          :key="index"
+          :to="tab.path"
+          :class="{ 'border-b-4 border-primary': $route.path === tab.path }"
+          class="hover:text-primary dark:hover:text-primary transition-all duration-150"
+        >
           {{ tab.name }}
         </RouterLink>
-        <a class="border p-2 border-blue-500 bg-blue-500 dark:border-blue-400 dark:bg-blue-400 rounded-3xl text-white"
-          href="/curriculum.docx" download>
+        <a
+          class="border p-2 border-primary bg-primary rounded-3xl transition-colors duration-150 text-white"
+          href="/curriculum.docx"
+          download
+        >
           Baixar curriculum
         </a>
         <ThemeTogglerComponent />
       </div>
 
       <div class="md:hidden flex items-center gap-x-4">
-        <a class="border p-2 border-blue-500 bg-blue-500 dark:border-blue-400 dark:bg-blue-400 rounded-3xl text-white"
-          href="/curriculum.docx" download>
+        <a
+          class="border p-2 border-primary bg-primary rounded-3xl transition-colors duration-150 text-white"
+          href="/curriculum.docx"
+          download
+        >
           Baixar curriculum
         </a>
         <ThemeTogglerComponent />
-        <a @click="toggleMenu()">
+        <a @click="toggleMenu()" href="#">
           <v-icon name="bi-list" scale="2" />
         </a>
       </div>
@@ -35,10 +47,16 @@
 
     <Transition name="slide-down" mode="out-in">
       <div v-if="isOpen" class="list bg-white dark:bg-dark-surface">
-        <div class="md:hidden flex flex-col items-center gap-y-3 border-b dark:border-dark-border">
-          <RouterLink v-for="(tab, index) in navTabs" :key="index" :to="tab.path"
-            :class="{ 'border-b-4 border-blue-500 dark:border-blue-400': $route.path === tab.path }"
-            class="p-2 transition-all duration-300">
+        <div
+          class="md:hidden flex flex-col items-center gap-y-3 border-primary"
+        >
+          <RouterLink
+            v-for="(tab, index) in navTabs"
+            :key="index"
+            :to="tab.path"
+            :class="{ 'border-b-4 border-primary': $route.path === tab.path }"
+            class="p-2 transition-all duration-150"
+          >
             {{ tab.name }}
           </RouterLink>
         </div>
@@ -46,7 +64,6 @@
     </Transition>
   </header>
 </template>
-
 
 <style>
 /* Animação para o slide-down */
@@ -70,22 +87,21 @@
 }
 </style>
 
-
 <script setup>
-import ThemeTogglerComponent from './ThemeTogglerComponent.vue'
-import { RouterLink } from 'vue-router'
-import { ref } from 'vue'
-const isOpen = ref(false)
+import ThemeTogglerComponent from "./ThemeTogglerComponent.vue";
+import { RouterLink } from "vue-router";
+import { ref } from "vue";
+const isOpen = ref(false);
 
 const navTabs = [
-  { name: 'Home', path: '/' },
-  { name: 'Sobre', path: '/about' },
-  { name: 'Projetos', path: '/projects' },
+  { name: "Home", path: "/" },
+  { name: "Sobre", path: "/about" },
+  { name: "Projetos", path: "/projects" },
   // { name: 'Trabalhos', path: '/works' },
-  { name: 'Contato', path: '/contact' },
-]
+  { name: "Contato", path: "/contact" },
+];
 
 const toggleMenu = () => {
-  isOpen.value = !isOpen.value
-}
+  isOpen.value = !isOpen.value;
+};
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,53 @@
 <template>
-  <div class="about dark:bg-dark-background dark:text-dark-text">
-
+  <div class="about my-auto dark:text-dark-text transition-all duration-150">
     <div
-      class="flex flex-col-reverse xl:flex-row items-center justify-between text-center xl:items-start xl:text-start gap-x-10">
+      class="flex flex-col-reverse xl:flex-row items-center justify-between text-center xl:items-start xl:text-start gap-x-10"
+    >
       <div class="info lg:w-3/5 mt-16 lg:mt-0">
-
-        <h1 class="flex flex-col lg:flex-row font-bold text-4xl sm:text-5xl xl:text-nowrap">
+        <h1
+          class="flex flex-col lg:flex-row font-bold text-4xl sm:text-5xl text-primary transition-colors duration-200 xl:text-nowrap"
+        >
           Desenvolvedor Fullstack
-          <span class="hidden lg:block">
-            🚀
-          </span>
-          <span class="lg:hidden">
-            😎
-          </span>
+          <span class="hidden lg:block"> 🚀 </span>
+          <span class="lg:hidden"> 😎 </span>
         </h1>
-        <h3 class="text-light dark:text-dark-text text-lg xl:w-4/5 mt-5">Olá! Sou Vinicius Spanol, desenvolvedor
-          fullstack com
-          experiência
-          na
-          criação de aplicativos web e mobile. Além disso, possuo conhecimentos em DevOps e Internet das Coisas (IoT),
-          sempre buscando soluções eficientes e inovadoras.
+        <h3 class="text-light dark:text-dark-text text-lg xl:w-4/5 mt-5">
+          Olá! Sou Vinicius Spanol, desenvolvedor fullstack com experiência na
+          criação de aplicativos web e mobile. Além disso, possuo conhecimentos
+          em DevOps e Internet das Coisas (IoT), sempre buscando soluções
+          eficientes e inovadoras.
         </h3>
 
         <div class="socials flex justify-center xl:justify-start gap-x-5 mt-6">
-          <a v-for="(icon, index) in contactIcons" :key="index" :href="icon.link" target="_blank"
-            class="dark:text-dark-text">
+          <a
+            v-for="(icon, index) in contactIcons"
+            :key="index"
+            :href="icon.link"
+            target="_blank"
+            class="text-primary"
+          >
             <v-icon :name="icon.icon" scale="2.5" />
           </a>
         </div>
 
         <div
-          class="stack flex flex-col lg:flex-row items-center gap-y-8 gap-x-5 mt-16 lg:mt-20 shadow-lg border dark:border-dark-border dark:bg-dark-surface rounded-xl p-5">
-          <p class="font-medium border-b-2 border-light dark:border-dark-border lg:border-0 lg:text-nowrap">Principais
-            tecnologias</p>
-          <div class="vertical-divisor hidden lg:block dark:text-dark-text">|</div>
+          class="stack flex flex-col lg:flex-row items-center gap-y-8 gap-x-5 mt-16 lg:mt-20 shadow-lg border dark:border-dark-border dark:bg-dark-surface rounded-xl p-5"
+        >
+          <p
+            class="font-medium border-b-2 border-light text-primary dark:border-dark-border lg:border-0 lg:text-nowrap"
+          >
+            Principais tecnologias
+          </p>
+          <div class="vertical-divisor hidden lg:block dark:text-dark-text">
+            |
+          </div>
 
           <div class="flex flex-wrap lg:flex-shrink justify-center gap-x-5">
-            <div v-for="(icon, index) in techStackIcons" :key="index"
-              class="flex hover:-translate-y-4 transition-all duration-150 cursor-pointer">
+            <div
+              v-for="(icon, index) in techStackIcons"
+              :key="index"
+              class="flex hover:-translate-y-4 cursor-pointer"
+            >
               <v-icon :name="icon.icon" scale="3" class="dark:text-dark-text" />
             </div>
           </div>
@@ -45,7 +55,6 @@
       </div>
       <HomeImageComponent />
     </div>
-
   </div>
 </template>
 
