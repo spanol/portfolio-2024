73eb8027c4959bf4274326cece4e8f907b1288c4
spanol <viniciusspanol@gmail.com>
2026-03-07 04:40:18 -0300

    feat: add new image asset and update background image format in HomeImageComponent

--- src/components/HomeImageComponent.vue
+++ src/components/HomeImageComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="z-10 img-hero bg-center m-auto bg-cover border-4 border-primary lg:mb-10 h-64 w-64 lg:h-72 lg:w-72 xl:h-96 xl:w-96"
-  ></div>
+    class="z-10 img-hero bg-center m-auto bg-cover border-4 border-primary lg:mb-10 h-64 w-64 lg:h-72 lg:w-72 xl:h-96 xl:w-96">
+  </div>
 </template>
 
 <style scoped>
 .img-hero {
   animation: morph 8s ease-in-out infinite;
-  background-image: url(/src/assets/spanol.jpg);
+  background-image: url(/src/assets/spanol.jpeg);
   background-position: 50%;
   background-repeat: no-repeat;
   position: relative;
